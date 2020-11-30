5f874a77e6805c0d068990399647eb1bdbe0c89e
Faustine wijaya <32315692+evelynwijaya@users.noreply.github.com>
2020-11-30 18:12:20 +0800

    latihan5/index.html

--- latihan5/js/init.js
+++ latihan5/js/init.js
@@ -5,3 +5,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.collapsible');
+   var instances = M.Collapsible.init(elems, options);
+ });
+
+ // Or with jQuery
+
+ $(document).ready(function(){
+   $('.collapsible').collapsible();
+ });
